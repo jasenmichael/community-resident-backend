64198593aa7c3c5772b92bbd08513f13bd210142
jasenmichael <jasen@ready-builder.com>
2018-12-16 20:42:49 -0600

    verify token passed in, use post method and request from body

--- src/routes/forms/work/list.php
+++ src/routes/forms/work/list.php
@@ -2,35 +2,42 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// get requires token in the header
-$app->get('/forms/work/list', function ($request, $response) {
-    $passedToken = array_values($request->getHeader('token'))[0];
+// post requires token in content-form or json body
+$app->post('/forms/work/list', function ($request, $response) {
+    // $passedToken = array_values($request->getHeader('token'))[0];
     require_once '../src/config/auth.php';
+    $body = $request->getParsedBody();
+    $passedToken = $body['token'];
+    require '../src/config/auth.php';
 
-    // get local token and compare
-    if(($token === $passedToken) || ($adminToken === $passedToken)) {
-        $sql = "SELECT * FROM work";
-
-        try{
-            $db = new db();
-            $db = $db->connect();
-
-            $statement = $db->query($sql);
-            $list = $statement->fetchAll(PDO::FETCH_OBJ);
-            $db = null;
-
-            return $response->withJson($list);
-        }
-        
-        catch(PDOException $e){
-            return $response->withJson(array('error' => 'could not access db'));
-        }
+    //check token was passed
+    if(!$passedToken){
+        return $response->withJson(array('error' => 'token required'));
     }
-    return $response->withJson(array('error' => 'invalid token'));
-    $db = null;
-    $token = null;
-    $passedToken = null;
-    $adminToken = null;
-    exit();
+
+    // verify token
+    if( !(password_verify($token, $passedToken)) || (password_verify($adminToken, $passedToken)) ){
+        return $response->withJson(array('error' => 'invalid token'));
+        exit();
+    }
+
+    // we good, do the thing
+    $sql = "SELECT * FROM work";
+    try{
+        $db = new db();
+        $db = $db->connect();
+
+        $statement = $db->query($sql);
+        $list = $statement->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        return $response->withJson($list);
+        exit();
+    }
+    
+    catch(PDOException $e){
+        return $response->withJson(array('error' => 'could not access db'));
+        exit();
+    }
 });
 

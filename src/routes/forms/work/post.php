69185427b3821a049e291e0939ad85f7a7348044
jasenmichael <jasen@ready-builder.com>
2018-12-16 20:49:14 -0600

    verify token passed in, use post method and request from body

--- src/routes/forms/work/post.php
+++ src/routes/forms/work/post.php
@@ -4,15 +4,15 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 // get requires token in the header
 $app->post('/forms/work/post', function ($request, $response) {
-    $passedToken = array_values($request->getHeader('token'))[0];
-    require_once '../src/config/auth.php';
+    $passedToken = $request->getParam('token');
+    require '../src/config/auth.php';
 
-    if( (password_verify($token, $passedToken)) || (password_verify($adminToken, $passedToken)) ){
-    // if(($token === $passedToken) || ($adminToken === $passedToken)) {
+    if(!$passedToken){
+        return $response->withJson(array('error' => 'token required'));
+        exit();
+    }
 
-      // we could just load the whole body like this,
-      // $body = json_decode($request->getBody(), true);
-      // but we want to explicitly construct the object
+    if( (password_verify($adminToken, $passedToken)) || (password_verify($token, $passedToken)) ){
       $body = array(
           'id' => 'placeholder',
           'resident_id' => $request->getParam('resident_id'),
@@ -23,42 +23,43 @@ $app->post('/forms/work/post', function ($request, $response) {
           'created_at' => date('Y-m-d H:i:s'),
           'updated_at' => date('Y-m-d H:i:s')
       );
-  
+
       // if any fields are null or empty, we reject the post
-      if (!in_array(null, $body) || !in_array("", $body) ){
-          // post body is valid
+        if (!in_array(null, $body) || !in_array("", $body) ){
+            // post body is valid
+            $sql = "INSERT INTO work (resident_id,service,date,hours,description,created_at,updated_at) VALUES
+            (:resident_id,:service,:date,:hours,:description,:created_at,:updated_at)";
 
-          $sql = "INSERT INTO resident (resident_id,service,date,hours,description,created_at,updated_at) VALUES
-          (:resident_id,:service,:date,:hours,:description,:created_at,:updated_at)";
+            try{
+                $db = new db();
+                $db = $db->connect();
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam(':resident_id', $body['resident_id']);
+                $stmt->bindParam(':service', $body['service']);
+                $stmt->bindParam(':date', $body['date']);
+                $stmt->bindParam(':hours', $body['hours']);
+                $stmt->bindParam(':description', $body['description']);
+                $stmt->bindParam(':created_at', $body['created_at']);
+                $stmt->bindParam(':updated_at', $body['updated_at']);
+                $stmt->execute();
+                // get the posted id and set body id(replacing the placeholder)
+                $body['id'] = $db->lastInsertId();
 
-          try{
-              // Get DB Object
-              $db = new db();
-              // Connect
-              $db = $db->connect();
-              $stmt = $db->prepare($sql);
-              $stmt->bindParam(':resident_id', $body['resident_id']);
-              $stmt->bindParam(':service', $body['service']);
-              $stmt->bindParam(':date', $body['date']);
-              $stmt->bindParam(':hours', $body['hours']);
-              $stmt->bindParam(':description', $body['description']);
-              $stmt->bindParam(':created_at', $body['created_at']);
-              $stmt->bindParam(':updated_at', $body['updated_at']);
-              $stmt->execute();
-
-              // get the posted id and set body id(replacing the placeholder)
-              $body['id'] = $db->lastInsertId();
-
-          } catch(PDOException $e){
-              echo '{"error": {"text": '.$e->getMessage().'}';
-          }
-          return $response->withJson($body);
-      } else {
-          return $response->withJson(array(
-              'error' => 'missing fields'
-          ));
-      }
+            } catch(PDOException $e){
+                return $response->withJson(array(
+                    'error' => 'could not access db',
+                    'message' => $e->getMessage()
+                ));
+                exit();
+            }
+            return $response->withJson($body);
+        } else {
+            return $response->withJson(array(
+                'error' => 'missing fields'
+            ));
+        }
     }
+    // else token invalid
     return $response->withJson(array('error' => 'invalid token'));
     $db = null;
     $token = null;

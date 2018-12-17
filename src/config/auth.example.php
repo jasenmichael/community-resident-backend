8f6f98e415ee81025a32a1dbb7124723edc18f25
jasenmichael <jasen@ready-builder.com>
2018-12-16 19:39:11 -0600

    edit example auth

--- src/config/auth.example.php
+++ src/config/auth.example.php
@@ -5,7 +5,7 @@ $adminToken = "some-sweet-admin-token-4k6j+n%fm-346j+n6gk%fm-kfmfma33kvt";
 
 $users = array(
   'resident' => password_hash('residentpass', PASSWORD_DEFAULT),
-  'admin' => password_hash('residentpass', PASSWORD_DEFAULT)
+  'admin' => password_hash('adminpass', PASSWORD_DEFAULT)
 );
 
 
